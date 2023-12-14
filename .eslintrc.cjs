6f0c2941a1b3c2211ece9def1ccfa56cc8f6d398
유형우/애버커스 <db6194@iabacus.co.kr>
2023-12-14 17:56:32 +0900

    eslint 설정

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,11 +5,24 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    "plugin:prettier/recommended",
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  parserOptions: {
+    "ecmaVersion": 6,
+    "sourceType": "module",
+    "ecmaFeatures": {
+      "jsx": true
+    }
+  },
+  plugins: ['react-refresh', 'prettier'],
   rules: {
+    'no-unused-vars': ['off'], // 사용하지 않는 변수가 있을때 빌드에러가 나던 규칙 해제
+    'arrow-parens': ['warn', 'as-needed'], // 화살표 함수의 파라미터가 하나일때 괄호 생략
+    'import/prefer-default-export': ['off'], // export const 문을 쓸때 에러를 내는 규칙 해제
+    'no-console': ['off'], // 콘솔을 쓰면 에러가 나던 규칙 해제
+    'prettier/prettier': 0,
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
