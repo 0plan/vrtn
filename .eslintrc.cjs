631f0f8c5b9267c1604ebf5256c140188a48e937
Lee, Chungho <chlee125@gmail.com>
2023-12-15 11:30:26 +0900

    feat: init storybook

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,19 +1,12 @@
 module.exports = {
   root: true,
   env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-    "plugin:prettier/recommended",
-  ],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:react-hooks/recommended', 'plugin:prettier/recommended', 'plugin:storybook/recommended'],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    "ecmaVersion": 6,
-    "sourceType": "module",
-    "ecmaFeatures": {
-      "jsx": true
+    'ecmaVersion': 6, 'sourceType': 'module', 'ecmaFeatures': {
+      'jsx': true
     }
   },
   plugins: ['react-refresh', 'prettier'],
@@ -22,10 +15,6 @@ module.exports = {
     'arrow-parens': ['warn', 'as-needed'], // 화살표 함수의 파라미터가 하나일때 괄호 생략
     'import/prefer-default-export': ['off'], // export const 문을 쓸때 에러를 내는 규칙 해제
     'no-console': ['off'], // 콘솔을 쓰면 에러가 나던 규칙 해제
-    'prettier/prettier': 0,
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
-  },
+    'prettier/prettier': 0, 'react-refresh/only-export-components': ['warn', { allowConstantExport: true }]
+  }
 }
