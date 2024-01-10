cac028ae28633e8cb847b7126cd84b47ba56b095
Lee, Chungho <chlee125@gmail.com>
2024-01-10 14:34:58 +0900

    fix: eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,7 +5,8 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
-    'plugin:storybook/recommended'
+    'plugin:storybook/recommended',
+    'eslint-config-airbnb'
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
@@ -14,16 +15,24 @@ module.exports = {
       'jsx': true
     }
   },
+  "settings": {
+    "import/resolver": {
+      "alias": {
+        map: [
+          [ "@components", "./src/components" ],
+          [ "~", "./src" ],
+        ]
+      }
+    }
+  },
   plugins: ['react-refresh'],
   rules: {
-    'import/prefer-default-export': ['off'], // export const 문을 쓸때 에러를 내는 규칙 해제
-    'no-console': ['off'], // 콘솔을 쓰면 에러가 나던 규칙 해제
     'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
     'react/jsx-filename-extension': 0,
     'react/react-in-jsx-scope': 0,
     'no-shadow': 0,
     'no-unresolved': 0,
-    "react/prefer-stateless-function": 0,
-    "react/jsx-one-expression-per-line": 0
+    'react/prefer-stateless-function': 0,
+    'react/jsx-one-expression-per-line': 0
   }
 }
