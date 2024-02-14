c09e4edd792f6b765debeeb500e2d89a42ff5fb2
유형우/애버커스 <db6194@iabacus.co.kr>
2024-02-14 09:51:35 +0900

    [feat] : react-md-editro 추가

---
+++ src/components/ui/markdown-editor.tsx
@@ -0,0 +1,27 @@
+import MDEditor from '@uiw/react-md-editor';
+import { useState } from 'react';
+import { useDarkMode } from '~/lib/dark-mode.ts';
+
+interface IProps {
+  height: number;
+  markdownMessage: string;
+}
+
+export default function MarkdownEditor({ height, markdownMessage }: IProps) {
+  const [value, setValue] = useState<string>(markdownMessage);
+  const changeCode = (code: string | undefined) => {
+    if (code !== undefined) setValue(code);
+  };
+  const { isDarkMode } = useDarkMode();
+  return (
+    <div className="container">
+      <div data-color-mode={isDarkMode ? 'light' : 'dark'}>
+        <MDEditor
+          value={value}
+          height={height}
+          onChange={(value) => changeCode(value)}
+        />
+      </div>
+    </div>
+  );
+}
---
+++ src/pages/example/markdown/editor.tsx
@@ -0,0 +1,23 @@
+import MarkdownEditor from '@components/ui/markdown-editor.tsx';
+
+export default function Editor() {
+  const mkdStr = `
+    # Markdown Editor
+    
+    ---
+    
+    **Hello world!!!**
+    
+    [![](https://avatars.githubusercontent.com/u/1680273?s=80&v=4)](https://avatars.githubusercontent.com/u/1680273?v=4)
+    
+    \`\`\`javascript
+    import React from "react";
+    import ReactDOM from "react-dom";
+    import MEDitor from '@uiw/react-md-editor'; 
+    
+    \`\`\`
+    `;
+  return (
+    <MarkdownEditor height={350} markdownMessage={mkdStr} />
+  );
+}
