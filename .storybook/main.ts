97ca7f329f1de7add46e9872ef82a92fee884019
Lee, Chungho <chlee125@gmail.com>
2024-03-20 15:58:57 +0900

    migrate: storybook 7 to 8

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -9,7 +9,8 @@ const config: StorybookConfig = {
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-onboarding",
-    "@storybook/addon-interactions"
+    "@storybook/addon-interactions",
+    "@storybook/addon-mdx-gfm"
   ],
   "framework": {
     "name": "@storybook/react-vite",
