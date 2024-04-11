2ccaefcdb58b6aba66db4902ec5e4759c830c8f6
Lee, Chungho <chlee125@gmail.com>
2024-04-11 16:18:07 +0900

    remove: butterfly-dag

--- src/pages/example/lineage/data-mapping.tsx
+++ src/pages/example/lineage/data-mapping.tsx
@@ -1,24 +1,7 @@
-import ButterflyDataMapping from 'react-data-mapping';
-import {
-  columns, mappingData, sourceData, targetData,
-} from '@/data/data-mapping/data';
-import 'react-data-mapping/dist/index.css';
-
 export default function DataMapping() {
   return (
-    sourceData && (
-    <ButterflyDataMapping
-      width={500}
-      height={500}
-      type="single"
-      columns={columns}
-      sourceData={sourceData}
-      targetData={targetData}
-      mappingData={mappingData}
-      className="butterfly-data-mappint"
-      sourceClassName="source-column"
-      targetClassName="target-column"
-    />
-    )
+    <div>
+      Data Mapping
+    </div>
   );
 }
