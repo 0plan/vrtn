e7c1c2a521e2345b994441a85cf4ff90c50a10ee
Lee, Chungho <31458303+chlee125@users.noreply.github.com>
2024-04-17 17:02:03 +0900

    feat: notFound page (#60)

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,8 +1,24 @@
+import { Button } from '@/components/ui/button';
+import { Link } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
+
 export default function NotFound() {
+  const { t } = useTranslation();
+
   return (
     <div id="error-page">
-      <h1>Oops!</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
+      <div className="relative overflow-hidden before:absolute before:start-1/2 before:top-0">
+        <div className="mx-auto max-w-[85rem] px-4 pb-10 pt-24 sm:px-6 lg:px-8">
+          <div className="mx-auto mt-5 max-w-2xl text-center">
+            <h2>{t('error.notFound.title')}</h2>
+            <p>{t('error.notFound.content')}</p>
+            <Link to="/">
+              <Button className="mt-4">{t('error.notFound.button')}</Button>
+            </Link>
+          </div>
+        </div>
+
+      </div>
     </div>
   );
 }
