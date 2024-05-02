47314dfa16349d13dd75f05fb2ecc34a30db6b06
Lee, Chungho <chlee125@gmail.com>
2024-05-02 16:10:51 +0900

    fix: domain

--- src/components/layouts/footer.tsx
+++ src/components/layouts/footer.tsx
@@ -7,7 +7,7 @@ function Footer() {
         <div className="space-x-2">
           <a
             className="inline-flex h-10 w-10 items-center justify-center rounded-full text-center text-gray-500 transition hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-blue-600 focus:ring-offset-2 focus:ring-offset-white dark:text-gray-500 dark:hover:bg-gray-800 dark:hover:text-gray-200"
-            href="https://github.com/chlee125/vite-react-typescript-template"
+            href="https://github.com/0plan/vrtn"
             target="_blank"
             rel="noreferrer"
           >
