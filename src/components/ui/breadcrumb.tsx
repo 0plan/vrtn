3767e4da54b524cf37f857d535100338e7479ff0
Lee, Chungho <chlee125@gmail.com>
2024-11-27 15:02:57 +0900

    chore: update packages

--- src/components/ui/breadcrumb.tsx
+++ src/components/ui/breadcrumb.tsx
@@ -81,7 +81,7 @@ function BreadcrumbSeparator({
     <li
       role="presentation"
       aria-hidden="true"
-      className={cn('[&>svg]:size-3.5', className)}
+      className={cn('[&>svg]:w-3.5 [&>svg]:h-3.5', className)}
       {...props}
     >
       {children ?? <ChevronRight />}
